e14254ba4a22e5af6201257e5722ba22ea06d0a3
GameXG <GameXG@Gmail.Com>
2015-12-31 22:20:47 +0800

    删除调试输出

--- directproxt_test.go
+++ directproxt_test.go
@@ -2,7 +2,6 @@ package proxyclient
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"net"
 	"testing"
@@ -16,54 +15,43 @@ func testDirectProxyTCP1(t *testing.T) {
 
 	p, err := newDriectProxyClient("",make(map[string][]string))
 	if err != nil {
-		t.Errorf("启动直连代理失败：%s", err)
-		fmt.Printf("%v %v", p, err)
+		t.Fatalf("启动直连代理失败：%s", err)
 		return
 	}
 
 	b := make([]byte, 30)
 	c, err := p.Dial("tcp", "127.0.0.1:13336")
 	if err != nil {
-		t.Errorf("代理连接失败：%v", err)
-	} else {
-		fmt.Println("连接成功")
+		t.Fatalf("代理连接失败：%v", err)
 	}
 	if _, err := c.Read(b); err != nil {
-		t.Errorf("代理读错误：%v", err)
-	} else {
-		fmt.Println("读b1成功")
+		t.Fatalf("代理读错误：%v", err)
 	}
 	if bytes.Equal(b[:len(B1)], B1) != true {
-		t.Errorf("数据不匹配：%v", err)
+		t.Fatalf("数据不匹配：%v", err)
 	}
 
 	if _, err := c.Write(B2); err != nil {
-		t.Errorf("写数据失败：%v", err)
-	} else {
-		fmt.Println("写b2成功")
+		t.Fatalf("写数据失败：%v", err)
 	}
 
 	if _, err := c.Read(b); err != nil {
-		t.Errorf("代理读错误：%v", err)
-	} else {
-		fmt.Println("读b3成功")
+		t.Fatalf("代理读错误：%v", err)
 	}
 
 	if bytes.Equal(b[:len(B3)], B3) != true {
-		t.Errorf("数据不匹配：%v", err)
+		t.Fatalf("数据不匹配：%v", err)
 	}
 
 	if err := c.Close(); err != nil {
-		t.Errorf("关闭连接错误。")
+		t.Fatalf("关闭连接错误。")
 	}
 }
 
 func TestDirectProxyTCP(t *testing.T) {
 	l, err := net.Listen("tcp", "127.0.0.1:13336")
 	if err != nil {
-		t.Errorf("错误,%v", err)
-	} else {
-		fmt.Println("监听成功")
+		t.Fatalf("错误,%v", err)
 	}
 
 	go testDirectProxyTCP1(t)
@@ -72,34 +60,26 @@ func TestDirectProxyTCP(t *testing.T) {
 
 	c, err := l.Accept()
 	if err != nil {
-		t.Errorf("接受连接错误：%v", err)
-	} else {
-		fmt.Println("接受链接成功")
+		t.Fatalf("接受连接错误：%v", err)
 	}
 
 	if _, err := c.Write(B1); err != nil {
-		t.Errorf("写数据失败：%v", err)
-	} else {
-		fmt.Println("写数据1成功")
+		t.Fatalf("写数据失败：%v", err)
 	}
 
 	if _, err := c.Read(b); err != nil {
-		t.Errorf("读错误：%v", err)
-	} else {
-		fmt.Println("读b2成功")
+		t.Fatalf("读错误：%v", err)
 	}
 	if bytes.Equal(b[:len(B2)], B2) != true {
-		t.Errorf("数据不匹配：%v", err)
+		t.Fatalf("数据不匹配：%v", err)
 	}
 
 	if _, err := c.Write(B3); err != nil {
-		t.Errorf("写数据失败：%v", err)
-	} else {
-		fmt.Println("写b3成功")
+		t.Fatalf("写数据失败：%v", err)
 	}
 
 	if _, err := c.Read(b); err != io.EOF {
-		t.Errorf("读结尾错误：%v", err)
+		t.Fatalf("读结尾错误：%v", err)
 	}
 
 }
